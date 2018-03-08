252ba91e8ee87e4dd6958f3cb88bc70a3521a54e
Jonathan Whiting <jonathan.whiting@pnnl.gov>
2018-03-08 13:33:12 -0800

    Prevents an error if the detection matrix was saved in Excel.

--- src/objects/E4DSensors.java
+++ src/objects/E4DSensors.java
@@ -146,7 +146,7 @@ public class E4DSensors {
 					String[] lineList = line.split(",");
 
 					// The first line lists the valid nodes per scenario (duplicates SensorSettings --> setValidNodes())
-					if (lineList.length!=0 && lineList[0].toLowerCase().equals(scenarios.get(scenarioIteration).getScenario())) {
+					if ((lineList.length!=0 && lineList[0].toLowerCase().equals(scenarios.get(scenarioIteration).getScenario())) || lineList[0].equals("")) {
 						orderedValidNodes.clear();
 						validNodes.clear();
 						for (int i=1; i<lineList.length; i++) {
