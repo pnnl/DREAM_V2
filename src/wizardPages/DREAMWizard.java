d2b381d0a21a831114a4ce643383ae87b1af4412
whit162 <jonathan.whiting@pnnl.gov>
2019-04-16 14:21:25 -0700

    Testing for Jar creation (this version will not run on Eclipse)

--- src/wizardPages/DREAMWizard.java
+++ src/wizardPages/DREAMWizard.java
@@ -79,7 +79,7 @@ public class DREAMWizard extends Wizard {
 		setWindowTitle(null);
 		setWindowTitle("DREAM Wizard");
 		setNeedsProgressMonitor(true);	
-		ImageData imgData = new ImageData("./img/dream.png"); 
+		ImageData imgData = new ImageData("./dream.png"); 
 		this.setDefaultPageImageDescriptor(ImageDescriptor.createFromImageData(imgData));
 		this.setTitleBarColor(new RGB(255,255,255));
 		data = new STORMData(this);
@@ -189,7 +189,7 @@ public class DREAMWizard extends Wizard {
 
 		final DREAMWizard wizard = new DREAMWizard();
 
-		WizardDialog.setDefaultImage(new Image(Display.getDefault(),"./img/icon.png"));
+		WizardDialog.setDefaultImage(new Image(Display.getDefault(),"./icon.png"));
 
 		WizardDialog wizardDialog = new WizardDialog(null, wizard) {
 			{
