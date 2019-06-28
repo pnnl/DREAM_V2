49344d9f2ac44c2cf1e5dc25374ac09492f3b98e
huan482 <brian.huang@pnnl.gov>
2019-06-28 15:23:24 -0700

    DREAM-74
    Correct coordinates are converted and stored. Just need to draw the map
    and we should have the right place on the window.

--- src/mapView/theMapView.java
+++ src/mapView/theMapView.java
@@ -1,9 +1,16 @@
 package mapView;
 
-import java.net.MalformedURLException;
+import java.awt.Graphics;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.imageio.ImageIO;
+
 import utilities.Constants;
 public class theMapView {
 	
@@ -17,48 +24,86 @@ public class theMapView {
 	
 	private static String myZoneDirection;
 	
-	private int myMinX;
+	private static int myOffsetX;
 	
-	private int myMinY;
+	private static int myOffsetY;
 	
 	private static double myLatitude;
 	
 	private static double myLongitude;
+	
+	private static Float myEasting;
+	
+	private static Float myNorthing;
 	
 //	private Viewer myViewer;
 	
 	
 	public theMapView(final List<IJ> theGridBoxes, final List<Float> xLines, final List<Float> yLines,
 			final int theZone, final String theZoneDirection, final int theOriginX, final int theOriginY) {
-		
+		myEasting = xLines.get(0); //The minimum element of the UTM coordinate.
+		myNorthing = yLines.get(0); 
 		myGridBoxes = theGridBoxes;
 		myListofXLines = new ArrayList<Float>(Constants.makeLines((ArrayList<Float>) xLines));
 		myListofYLines = new ArrayList<Float>(Constants.makeLines((ArrayList<Float>) yLines));
 		myZone = theZone;
 		myZoneDirection = theZoneDirection;
-		myMinX = theOriginX;
-		myMinY = theOriginY;
+		myOffsetX = theOriginX;
+		myOffsetY = theOriginY;
 //		System.out.println(myListofXLines.size()+ " " + myListofYLines.size());
 	}
 	
 	public static void main (final String theArgs[]) {
-		buildMap();
+		
 	}
 	
-	private static void buildMap() {
+	public void paintComponent(final Graphics theGraphic) {
+		buildMap(theGraphic);
+		
+	}
+	/**
+	 * First convert given UTM coordinates to Lat/Long then find the correct google map location.
+	 * Build Map URL after this.
+	 * @author huan482
+	 * @date 6/28/2019
+	 */
+	private static void buildMap(final Graphics theGraphic) {
 		CoordinateConversion converter = new CoordinateConversion();
-		double[] myLatLong = converter.utm2LatLon(myZone + " " + myZoneDirection); // TODO parse this correctly...
+		String[] temp = converter.latLon2UTM(myOffsetX, myOffsetY).split(" ");
+		myEasting = myEasting + Float.parseFloat(temp[2]);
+		myNorthing = myNorthing + Float.parseFloat(temp[3]);
+		double[] myLatLong = converter.utm2LatLon(myZone + " " + myZoneDirection + " " + myEasting
+				+ " " + myNorthing); 
 		myLatitude = myLatLong[0];
+		if (myLatitude > 90) {
+			myLatitude = (myLatitude - 180);
+		} else if (myLatitude < -90) myLatitude = (myLatitude + 180);
+		
 		myLongitude = myLatLong[1];
 		int initZoom = 50;
-		
 		try {
 			URL map = new URL("https://maps.googleapis.com/maps/api/staticmap?center=" + myLatitude + "," + myLongitude
 					+ "&zoom=" + initZoom +
 					"&size=640x640&maptype=satellite&scale=2&key=AIzaSyCqMjOt2Q17PnE9-9843sutOpihbglC_6k");
-		} catch (MalformedURLException theException) {
+			BufferedImage image = ImageIO.read(map);
+			AffineTransform at = new AffineTransform();
+			AffineTransformOp scaleOp = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
+			image = scaleOp.filter(image, image);
+			theGraphic.drawImage(image, 0, 0, null);
+		} catch (IOException theException) {
 			theException.printStackTrace();
 		}
 	}
+	
+//	private void findArea() {
+//		if(myGridBoxes == null) {
+//			boxes = new ArrayList<IJ>();
+//			for(int i=1; i< xlines.size(); i++){
+//				for(int j=1; j< ylines.size(); j++){ //Only iterate through size-1 as we care about boxes, not about corners
+//					boxes.add(new IJ(i,j, true, true));
+//				}
+//			}
+//		}
+//	}
 	
 }
