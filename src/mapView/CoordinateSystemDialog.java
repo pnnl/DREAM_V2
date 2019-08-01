4aab9e3ed1e1db1f490da90670e5e6cd6f43d8f6
huan482 <brian.huang@pnnl.gov>
2019-08-01 11:18:36 -0700

    More bug fixes.

--- src/mapView/CoordinateSystemDialog.java
+++ src/mapView/CoordinateSystemDialog.java
@@ -268,7 +268,7 @@ public class CoordinateSystemDialog extends TitleAreaDialog {
 		} else {
 			myEnableButtonCheck[3] = true;
 		}
-		if (offsetRequired) {
+		if (isIncludeLocations) {
 			createDirectoryGUI();
 		}
 	}
