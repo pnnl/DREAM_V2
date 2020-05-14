cbe1ad8d32feb796a3b9ada009d70b1d37a6c784
whit162 <jonathan.whiting@pnnl.gov>
2020-05-14 08:39:59 -0700

    Replace language in disclaimer.

--- src/wizardPages/DREAMWizard.java
+++ src/wizardPages/DREAMWizard.java
@@ -190,9 +190,15 @@ public class DREAMWizard extends Wizard {
 		
 		// Pop up the disclaimer, exit on cancel
 		MessageBox messageBox = new MessageBox(shell, SWT.OK | SWT.CANCEL );
-		messageBox.setMessage("The Software was produced by Battelle under Contract No. DE-AC05-76RL01830 with the Department of Energy.  The U.S. Government is granted for itself and others acting on its behalf a nonexclusive, paid-up, irrevocable worldwide license in this data to reproduce, prepare derivative works, distribute copies to the public, perform publicly and display publicly, and to permit others to do so.  The specific term of the license can be identified by inquiry made to Battelle or DOE."
-				+ "\n\nNeither the United States Government, nor any of their employees, makes any warranty, express or implied, or assumes any legal liability or responsibility for the accuracy, completeness, or usefulness of any information, apparatus, product, or process disclosed, or represents that its use would not infringe privately owned rights. Reference herein to any specific commercial product, process, or service by trade name, trademark, manufacturer, or otherwise does not necessarily constitute or imply its endorsement, recommendation, or favoring by the United States Government or any agency thereof. The views and opinions of authors expressed herein do not necessarily state or reflect those of the United States Government or any agency thereof.");
-		messageBox.setText("NOTICE TO USERS");
+		messageBox.setMessage("This material was prepared as an account of work sponsored by an agency of the United States Government.  Neither the United States Government nor the United States Department of Energy, nor Battelle, nor any of their employees, nor any jurisdiction or organization that has cooperated in the development of these materials, makes any warranty, express or implied, or assumes any legal liability or responsibility for the accuracy, completeness, or usefulness or any information, apparatus, product, software, or process disclosed, or represents that its use would not infringe privately owned rights."
+				+ "\n\nReference herein to any specific commercial product, process, or service by trade name, trademark, manufacturer, or otherwise does not necessarily constitute or imply its endorsement, recommendation, or favoring by the United States Government or any agency thereof, or Battelle Memorial Institute. The views and opinions of authors expressed herein do not necessarily state or reflect those of the United States Government or any agency thereof."
+				+ "\n\n\t\t PACIFIC NORTHWEST NATIONAL LABORATORY"
+				+ "\n\t\t\t\u0020\u0020\u0020\u0020\u0020\u0020 operated by"
+				+ "\n\t\t\t\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020 BATTELLE"
+				+ "\n\t\t\t\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020 for the"
+				+ "\n\t\t\u0020\u0020\u0020 UNITED STATES DEPARTMENT OF ENERGY"
+				+ "\n\t\t\u0020\u0020\u0020\u0020\u0020\u0020\u0020 under Contract DE-AC05-76RL01830");
+		messageBox.setText("General Disclaimer");
 		int response = messageBox.open();
 		if (response == SWT.CANCEL)
 			System.exit(0); // Exit if they don't accept
